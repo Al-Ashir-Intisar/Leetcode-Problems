faa6356c4a13553aa8ebcd746c0ff9e4c5e2c206
Al Ashir Intisar <109549706+Al-Ashir-Intisar@users.noreply.github.com>
2023-11-23 22:04:13 +0000

    completed longest sub string problem better solution

--- longest_sub_str.py
+++ longest_sub_str.py
@@ -45,10 +45,27 @@ class Solution(object):
         #print(temp)
         #print(character_values)
         return max
-
+    #better function solution
+    def lengthOfLongestSubstring1(self, s: str) -> int:
+        n = len(s)
+        maxLength = 0
+        charSet = set()
+        left = 0
+        
+        for right in range(n):
+            if s[right] not in charSet:
+                charSet.add(s[right])
+                maxLength = max(maxLength, right - left + 1)
+            else:
+                while s[right] in charSet:
+                    charSet.remove(s[left])
+                    left += 1
+                charSet.add(s[right])
+        
+        return maxLength
 
 
 
 
 solution = Solution()
-solution.lengthOfLongestSubstring("abcabcbb")
+print(solution.lengthOfLongestSubstring1("abcabcbb"))
