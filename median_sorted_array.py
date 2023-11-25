dbc1de805e9facb9dc8082f20a20fe1634bc2e4e
Al Ashir Intisar <109549706+Al-Ashir-Intisar@users.noreply.github.com>
2023-11-25 04:42:33 +0000

    median of two sorted arrays problem solved

---
+++ median_sorted_array.py
@@ -0,0 +1,52 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        merged_arr = []
+        size_m = len(nums1)
+        size_n = len(nums2)
+        median = None
+        merged_lenght = size_m + size_n
+        i = 0
+        j = 0
+        while i < size_m or j < size_n:
+            next_m = nums1[i]
+            next_n = nums2[j]
+            if next_m < next_n:
+                merged_arr.append(next_m)
+                print(next_m)
+                i += 1
+                print(i)
+                if i == size_m:
+                    while j < size_n:
+                        merged_arr.append(nums2[j])
+                        print(nums2[j])
+                        j += 1
+                        print(j)
+            else:
+                merged_arr.append(next_n)
+                print(next_n)
+                j += 1
+                print(j)
+                if j == size_n:
+                    while i < size_m:
+                        merged_arr.append(nums1[i])
+                        print(nums1[i])
+                        i += 1
+                        print(i)
+        print(merged_arr)
+
+        if merged_lenght%2 == 1:
+            median = merged_arr[merged_lenght//2]
+            print(median)
+        else:
+            median = (merged_arr[(merged_lenght//2)-1] + merged_arr[merged_lenght//2])/2
+            print(median)
+        return median        
+
+
+
+
+solution = Solution()
+nums1 = [1,2]
+nums2 = [3,4]
+
+solution.findMedianSortedArrays(nums1, nums2)
